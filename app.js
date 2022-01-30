728fafb0a01f640e827000cd7034e26d972ede86
suraj9060 <87435542+suraj9060@users.noreply.github.com>
2022-01-30 17:48:41 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,42 @@
+const bar = document.getElementById("bar")
+const nav = document.getElementById("navbar");
+const close = document.getElementById("close")
+
+if (bar) {
+    bar.addEventListener('click', () => {
+        nav.classList.add('active')
+    })
+
+    
+}
+
+if (close) {
+    close.addEventListener('click', () => {
+        nav.classList.remove('active')
+    })
+}
+
+
+//page img change
+
+var MainImg = document.getElementById("MainImg");
+var smallImg = document.getElementsByClassName("small-img");
+
+// for (var i = 0; i < smallImg.length; i++){
+//     smallImg[0].onclick = function () {
+//         MainImg.src = smallImg[0].src;
+//     }
+// }
+ smallImg[0].onclick = function () {
+   MainImg.src = smallImg[0].src;
+};
+  smallImg[1].onclick = function () {
+    MainImg.src = smallImg[1].src;
+};
+   smallImg[2].onclick = function () {
+     MainImg.src = smallImg[2].src;
+};
+    smallImg[3].onclick = function () {
+      MainImg.src = smallImg[3].src;
+};
+    
